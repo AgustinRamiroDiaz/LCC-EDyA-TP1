6ddab490525f19a75229cc7a447d8ad78447ef23
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2020-04-21 18:36:22 -0300

    refactor segun hlint

--- main.hs
+++ main.hs
@@ -1,7 +1,5 @@
 {-# LANGUAGE FlexibleInstances      #-}
 {-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE MultiParamTypeClasses  #-}
-{-# LANGUAGE TemplateHaskell        #-}
 
 module TTree where
 
@@ -23,7 +21,7 @@ instance Ord k => Dic [k] v (TTree k v) where
   eliminar = delete
   claves = keys
 
---empty :: devuelve un arbol vacio.
+--empty :: devuelve un árbol vacio.
 empty :: TTree k v
 empty = E
 
@@ -39,7 +37,7 @@ search (caracter:clave) (Node k v i m d) | caracter == k = search clave m
                                          | caracter < k = search (caracter:clave) i
                                          | caracter > k = search (caracter:clave) d
 
---insert :: agrega un par (clave, valor) a un arbol. Si la clave ya está en el ́arbol, actualiza su valor
+--insert :: agrega un par (clave, valor) a un árbol. Si la clave ya está en el ́arbol, actualiza su valor
 insert :: Ord k => [k] -> v -> TTree k v -> TTree k v
 insert [caracter] valor E = Leaf caracter valor
 insert (caracter:clave) valor E = Node caracter Nothing E (insert clave valor E) E
@@ -59,34 +57,34 @@ insert (caracter:clave) valor (Node k v i m d) | caracter == k = Node k v i (ins
 -- TODO: optimizar?
 --merge :: combina dos arboles xd
 merge :: TTree k v -> TTree k v -> TTree k v
-merge E a                = a
-merge l@(Leaf k v) E     = l
-merge (Leaf k v) a       = Node k (Just v) E E a
+merge E a = a
+merge l@(Leaf k v) E = l
+merge (Leaf k v) a = Node k (Just v) E E a
 merge (Node k v i m d) a = Node k v i m (merge d a)
 
---delete elimina una clave y el valor asociada a ́esta en un  ́arbol.
+--delete :: elimina una clave y el valor asociada a ́esta en un  arbol.
 delete :: Ord k => [k] -> TTree k v -> TTree k v
-delete clave E                 = E
+delete clave E = E
 delete [caracter] l@(Leaf k v) = if caracter == k then E else l
-delete _ l@(Leaf k v)          = l
-delete [caracter] (Node k v i m d) | caracter == k = (case m of E -> merge i d
-                                                                _ -> Node k Nothing i m d)
+delete _ l@(Leaf k v) = l
+delete [caracter] (Node k v i m d) | caracter == k = case m of E -> merge i d
+                                                               _ -> Node k Nothing i m d
                                    | caracter < k = Node k v (delete [caracter] i) m d
                                    | caracter > k = Node k v i m (delete [caracter] d)
-delete (caracter:clave) (Node k v i m d) | caracter == k = (case v of Nothing -> (let resultado = (delete clave m) in (case resultado of E -> merge i d
-                                                                                                                                         _ -> Node k v i resultado d))
-                                                                      Just _ -> Node k v i (delete clave m) d)
+delete (caracter:clave) (Node k v i m d) | caracter == k = case v of Nothing -> let resultado = delete clave m in case resultado of E -> merge i d
+                                                                                                                                    _ -> Node k v i resultado d
+                                                                     Just _ -> Node k v i (delete clave m) d
                                          | caracter < k = Node k v (delete (caracter:clave) i) m d
                                          | caracter > k = Node k v i m (delete (caracter:clave) d)
 
---keys :: dado un  ́arbol devuelve una lista ordenada con las claves del mismo.
+--keys :: dado un ́arbol devuelve una lista ordenada con las claves del mismo.
 keys :: TTree k v -> [[k]]
-keys E                = []
-keys (Leaf k v)       = [[k]]
-keys (Node k v i m d) = (keys i) ++ map (k:) (keys m) ++ (keys d) ++ (case v of Nothing -> []
-                                                                                Just _ -> [[k]])
+keys E = []
+keys (Leaf k v) = [[k]]
+keys (Node k v i m d) = keys i ++ map (k:) (keys m) ++ keys d ++ case v of Nothing -> []
+                                                                           Just _ -> [[k]]
 
-ttreeToTree :: (Show k, Show v) => TTree k v -> Data.Tree.Tree [Char]
+ttreeToTree :: (Show k, Show v) => TTree k v -> Data.Tree.Tree String
 ttreeToTree E = Data.Tree.Node "" []
 ttreeToTree (Leaf k v) = Data.Tree.Node (show k ++ " " ++ show v) []
 ttreeToTree (Node k Nothing i m d) = Data.Tree.Node (show k) (map ttreeToTree [i, m, d])
